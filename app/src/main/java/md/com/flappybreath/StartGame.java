b14c0e20168ce8e41063443ff2b66acc51cb0cfc
Gautham <gauthammd@Gauthams-MacBook-Air.local>
2018-10-17 12:28:56 +0100

    Initial commit

---
+++ app/src/main/java/md/com/flappybreath/StartGame.java
@@ -0,0 +1,16 @@
+package md.com.flappybreath;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class StartGame extends Activity {
+
+    GameView gameView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+}
